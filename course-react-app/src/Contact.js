1e5a52b698e4d0dd4ea99197e346165018caaff8
maksvet <maksuttt@gmail.com>
2021-03-09 15:37:48 -0500

    WIP

--- course-react-app/src/Contact.js
+++ course-react-app/src/Contact.js
@@ -1,21 +1,50 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+
  
-class Contact extends Component {
-  render() {
-    return (
-      <form onsubmit="return false">
+// class Contact extends Component {
+//   render()
+const Contact = (props) => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const [content, setContent] = useState("")
+  const formSubmit = async event => {
+    event.preventDefault()
+    const response = await fetch('http://localhost:3007/contact_form/entries', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({name, email, phoneNumber, content})
+        })
+        const payload = await response.json()
+        if (response.status !== 200) {
+            alert(`Error: ${payload.message} for fields: ${payload.invalid.join(",")}`)
+        } else {
+            alert(`Your message successfully submitted`)
+        }
+  }
+
+
+    return(
+      <form onSubmit={formSubmit}>
       <label for="name"></label>
-      <input type="text" id="name" name="name" placeholder="Your name..." required/>
+      <input type="text" id="name" name="name" placeholder="Your name..." required value={name} onChange={e => setName(e.target.value)}/>
   
       <label for="email"></label>
-      <input type="email" id="email" name="email" placeholder="Your email..." required/>
+      <input type="email" id="email" name="email" placeholder="Your email..." required value={email} onChange={e => setEmail(e.target.value)}/>
+
+      <label for="phoneNumber"></label>
+      <input type="phoneNumber" id="phoneNumber" name="phoneNumber" placeholder="Your phone number..." required value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)}/>
   
       <label for="message"></label>
-      <textarea id="message" name="message" placeholder="Your message..." required></textarea>
-    <input type="submit" value="Submit" onclick="messsageSent()"/>
+      <textarea id="message" name="message" placeholder="Your message..." required value={content} onChange={e => setContent(e.target.value)}></textarea>
+    <input type="submit" value="Submit"/>
     </form>
-    );
-  }
+   )
+    
+  
 }
  
 export default Contact;
--- course-react-app/src/Hobbies.js
+++ course-react-app/src/Hobbies.js
@@ -8,19 +8,19 @@ class Hobbies extends Component {
         <p>I have too many hobbies, so little time.</p>
         <p>Just to name a few:</p>
         <ul>
-          <li class="hobbies">Unicycling (is the most recent one)</li>
-          <li class="hobbies">Whitewater canoeing </li>
-          <li class="hobbies">Fishing/icefishing: I fish for jumbo perch.</li> 
-          <li class="hobbies">Vintage snowmobiling</li> 
+          <li className="hobbies">Unicycling (is the most recent one)</li>
+          <li className="hobbies">Whitewater canoeing </li>
+          <li className="hobbies">Fishing/icefishing: I fish for jumbo perch.</li> 
+          <li className="hobbies">Vintage snowmobiling</li> 
           <img src="images/20150124_125914.jpg" alt="Ski-Doo Elan snowmobile"/>
-          <li class="hobbies">Moped racing</li>
+          <li className="hobbies">Moped racing</li>
           <img src="images/P1100222.JPG" alt="Moped racing in NY, US"/>
           <img src="images/P1100235.JPG" alt="Moped racing in NY, US"/>
-          <li class="hobbies">Model airplanes</li>
-          <li class="hobbies"> Music </li> 
-          <li class="hobbies">Graphic art</li>
+          <li className="hobbies">Model airplanes</li>
+          <li className="hobbies"> Music </li> 
+          <li className="hobbies">Graphic art</li>
         </ul>
-        <div class="video">
+        <div className="video">
         <iframe width="420" height="315" src="https://www.youtube.com/embed/5SvNzWqmgEM"
         title="control line airmodels combat video">
         </iframe>
