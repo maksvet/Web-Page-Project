0bf6009baa0daf156c7f15593535ccc9e541c1ac
maksvet <maksuttt@gmail.com>
2021-05-01 00:17:05 -0400

    WIP routes and front

--- course-react-app/src/Resume.js
+++ course-react-app/src/Resume.js
@@ -1,7 +1,18 @@
-import React, { Component } from "react";
- 
-class Resume extends Component {
-  render() {
+import React, { useState, useEffect } from "react";
+
+
+const Resume = () => {
+  const [resume, setResume] = useState([]);
+  
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("http://localhost:3008/resume");
+      res.json().then((res) => setResume(res));
+      //.catch((err) => setErrors(err));
+    }
+    fetchData();
+  }, []);
     return (
       <article>
 
@@ -9,127 +20,18 @@ class Resume extends Component {
 
   <h3>HIGHLITES OF QUALIFICATIONS:</h3>
   <ul class="skills">
-    <li class="resume">Programming: HTML5, CSS3, JavaScript.</li>
-    <li class="resume">Tools/Methodologies: Visual Studio Code, Git, Node JS, Express, React.</li>
-    <li class="resume">Operating Systems: Windows, MAC OS.</li>
-    <li class="resume">Others: Adobe Photoshop, Autodesk AutoCAD, Gravograph's CNC software, SAP, MS Project, Microsoft Office Tools.</li>
-    <li class="resume">Excel in targeting technical problems, finding corrective actions, and identifying true root causes.</li> 
-    <li class="resume">Qualified for Shop Certification Authority maintenance release TC Form One, CCAR-145, military components certification.</li>
-    <li class="resume">Proficient in AS9100, ISO9001 and other relevant aerospace industry standards; reading, interpreting, and applying
-     data, including blueprints, engineering drawings, product specifications, and technical manuals. </li>
-    <li class="resume">Experienced in working with precision measuring tools such as calipers; micrometers; dial/digital bore gauges; surface
-     meters; dial/test indicators; Rockwell scale hardness testers; mechanical and digital manometers and gauges.</li>
-    <li class="resume">Skillful in testing hydraulic, pneumatic and electrical components.</li>
-    <li class="resume">Specialize in repair & overhaul of electrical, pneumatic, hydraulic, and mechanical aircrafts components.</li>
-    <li class="resume">Expertise in operating, maintenance, and calibrating of SCHENCK dynamic balancing system.</li>
-    <li class="resume">Prior work history with hand and power tools; CNC engraving machines; industrial machines: lathes, milling machines,
-     grinders, brakes; MIG & TIG welding; plasma cutting; vehicle upholstery; media blasting, metal surfaces preparation and painting;
-     2-4 strokes power plants repair.</li>
-    <li class="resume">Attentive to detail and follow through.</li>
+        <div id="Entries">
+            {resume.map((item, key) => (
+                <li key={item.id}>
+                {item.name}; Phone {item.phone}; Email: {item.email}; Address: {item.street_address};
+                </li>
+            ))}
+        </div>
     <li class="resume">Energetic, hardworking, punctual, creative and able to work at various shifts.</li>
 </ul>
-<h3>WORK EXPERIENCE:</h3>
-
-<p> <strong> Quality Inspector, Repair and Overhaul Department	January 2011 – current</strong> <br/>
-Safran Landing Systems, Ajax, Ontario</p>
-
-<p class="text">
-&#9734; Performing inspections for aircraft landing gears and relating components.<br/>
-&#9734; Assessment and approval of work performed by fitters during overhauls and repairs; detailed visual inspection, measuring<br/>
-inspection and troubleshooting of landing gears and landing gears’ components<br/> class="resume"
-&#9734; Performing Sub-Contract Receiving Inspection: visual and dimensional inspection, certification approval. Maintain, control<br/>
-and process non-conforming parts.<br/>
-&#9734; Responsible for product compliance to the relevant specifications, drawings, CMMs and customer requirements.<br/>
-&#9734; Issuing Shop Certification Authority maintenance release TC Form One, CCAR-145, military components certification.<br/>
-&#9734; Testing shock assemblies and hydraulic components in accordance with maintenance procedures. Troubleshooting relevant problems, <br/>
-finding root cause.<br/>
-&#9734; Completing Deviation Reports, Rework Tickets, and NCRs for components deviating from Technical Data requirement at any stage of<br/>
-the repair process.<br/>
-&#9734; Creating Concessions Reports for parts with issues outside of specified repairs. Coordinating repair schemes and layouts with layouts <br/>
-specialist and OEM engineering department.<br/>
-&#9734; Preparing Failure Analysis Reports for components returning to service.<br/>
-&#9734; Maintaining traceability of parts for sterilized components; managing internal sterilized parts’ database.<br/>
-&#9734; Controlling disposal of scrapped components, preparing Certificates of Destruction.<br/>
-&#9734; Reporting to Repair & Overhaul management, quality department and senior staff recurring-unresolved issues, including unsatisfactory<br/>
-&#9734; trends or weaknesses in the quality/inspection systems.<br/>
-&#9734; Participating in 5S and FOD Awareness & Prevention programs: shop floor layout redesigning, work stations improving, FOD controlling<br/>
-in the department<br/>
-</p>
-
-
-
-<p><strong> Fitter, Repair and Overhaul Department	April 2008 – January 2011</strong> <br/>
-
-Safran Landing Systems, Ajax, Ontario <br/></p>
-<p class="text">
-  &#9734; Overhauling, repairing and modifying; detailed visual inspection, measuring inspection and troubleshooting of <br/>
-landing gears and landing gears’ components <br/>
-&#9734; Working process documentation and technical database using (CMM database and technical drawings database PDM4)<br/>
-&#9734; Completing paperwork and systems records for traceability of serialized parts, incorporated service bulletins and<br/>
-modifications; preparing reports for first, “in process” and final inspection; preparing documents for engineering concessions.<br/>
-&#9734; Collaborating with inspection and engineering specialists in preparation of repair layouts for various landing gear components.<br/>
-&#9734; Reporting to Repair and Overhaul management, quality department and senior staff recurring-unresolved issues, including <br/>
-unsatisfactory trends or weaknesses in the quality/inspection systems.<br/>
-&#9734; Lead hand assisting, coworkers training<br/>
-&#9734; Participating in 5S and FOD Awareness & Prevention programs: shop floor layout redesigning, work stations improving, <br/>
-&#9734; FOD controlling in the department<br/>
-</p>
-
-<p> <strong> Aircraft components technician, Sept. 2005 – Apr. 2008 </strong><br/>
-
-Global Aerospace Corporation, <br/>
-Mississauga, Ontario.</p>
-
-<p class="text">
-
-  &#9734; Overhauling and repairing, assembling and disassembling, visual and measuring inspection, troubleshooting, <br/>
-solving complicated mechanical problems and final testing of pneumatic, mechanical, electrical and hydraulic <br/>
-aircrafts components<br/>
-&#9734; Rotation groups balancing on SCHENCK GAB 803R balancing machine, setups and fixtures designing.<br/>
-&#9734; Vibration level measuring with Reliability Direct Vibration meters, designing vibration test fixtures <br/>
-&#9734; High-speed rotation components team leading and co-workers training. Managing one's own time and the time of coworkers.<br/>
-&#9734; Working process documentation and technical database using.<br/>
-&#9734; Recommending product and blueprint improvements <br/>
-&#9734; Collaborating with Global Engineering Corporation specialists and Transport Canada inspectors to perform Hamilton <br/>
-&#9734; Sundstrand&#9734ter generator’s adaptor (#6430587-1GE) failure test and ACM #782790 series dynamic tests. <br/>
-&#9734; Cooperating in Scientific Research and Experimental Development program. <br/>
-&#9734; Special tools designing and improving.<br/>
-&#9734; History of investigating customer complains and deficiency reports.<br/>
-&#9734; High pressure pneumatic test stand using, leakage testing, flow measuring.<br/>
-&#9734; Fixtures and special tools fabricating. <br/>
-</p>
-
-
-<h3> EDUCATION:</h3>                                                                                      
-<ul class="skills">
-
-<li class="resume">Cerificate in Full-Stack Web Developement. In progress; York University, Toronto, ON, Canada</li>  
-
-<li class="resume">Mathematics for Computer Science. Sept. 2020; Online course authorized by University of London, Goldsmiths, <br/>
-  University of London and offered through Coursera</li>
-
-<li class="resume">Contract Law: From Trust to Promise to Contract. Aug. 2020; Course of study by HarvardX, Harvard University</li>
-
-<li class="resume">Diploma in Aircraft Mechanics, 2006; Education Direct, Westmount, QC, Canada.</li>
-
-<li class="resume">CAR 573 Regulations Training, 2020; HPA Consultants, Ajax, ON</li>
-
-<li class="resume">CAR 561 Regulations Training, 2020; HPA Consultants, Ajax, ON</li>
-
-<li class="resume">Chinese Civil Aviation Regulation 145, 2020; HPA Consultants, Ajax, ON</li>
-
-<li class="resume">Human Factors in Aviation Maintenance, 2019; HPA Consultants, Ajax, ON</li>
-</ul>
-
-<h3>LANGUAGES:</h3> 
-  <p>English, Russian.</p>
-
-<h3>Interests</h3> 
-<p>Model airplanes flying (F2D combat); whitewater canoeing; vintage snowmobiles and fishing
-</p>
 </article>
     );
   }
-}
+
  
 export default Resume;
